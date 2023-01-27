2d97a3e47194307be112c72b6075b3ea35a8d0e9
hheeju98 <hheeju98@naver.com>
2023-01-27 13:37:25 +0900

    Styles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/abot-us" element={<h1>Hello</h1>} />
         <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
       </Routes>
     </Router>
   );
